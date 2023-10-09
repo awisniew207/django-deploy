320a45b1feba29940263e2cfb124ece13c8fb6a2
awisniew207 <awisniew@uoregon.edu>
2023-10-09 12:11:59 -0700

    Barber App

--- LineUp/Barber/views.py
+++ LineUp/Barber/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This works")
+    return render(request, 'Barber\index.html')
+
+def respond(request):
+    return HttpResponse("Made it")
--- LineUp/LineUp/urls.py
+++ LineUp/LineUp/urls.py
@@ -22,4 +22,5 @@ from Barber import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('madeit/', views.respond),
 ]
