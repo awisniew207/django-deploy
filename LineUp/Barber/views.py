d6d88d04b0f5305f46fcadc920a64158e2d84175
joshmv23 <127642400+joshmv23@users.noreply.github.com>
2023-10-11 11:56:28 -0700

    idk

--- LineUp/Barber/views.py
+++ LineUp/Barber/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'Barber\index.html')
+    return render(request, 'Barber/index.html')
 
 def respond(request):
     return HttpResponse("Made it")
