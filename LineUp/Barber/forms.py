3069635993a52d878116695206607764350db09d
awisniew207 <awisniew@uoregon.edu>
2023-11-02 18:54:59 -0700

    Barber Login

--- LineUp/Barber/forms.py
+++ LineUp/Barber/forms.py
@@ -6,21 +6,9 @@ from .models import User, Customer, Barber
 from django.contrib.auth.forms import AuthenticationForm
 
 class CustomerSignUpForm(UserCreationForm):
-    password1 = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput,
-        min_length=6,  # Set a custom minimum password length
-        error_messages={
-            'min_length': 'Password must be at least 6 characters long.'
-        }
-    )
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput, validators=[])
 
-    email = forms.EmailField(
-        label="Email",
-        error_messages={
-            'invalid': 'Enter a valid email address.',
-        }
-    )
+    email = forms.EmailField(label="Email")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -82,23 +70,9 @@ class ProfileForm(forms.ModelForm):
 
 #-------------------------------------------------------------------------------
 class BarberSignUpForm(UserCreationForm):
-    password1 = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput,
-        min_length=6,  # Set a custom minimum password length
-        error_messages={
-            'min_length': 'Password must be at least 6 characters long.'
-        }
-    )
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput, validators=[])
 
-    email = forms.EmailField(
-        label="Email",
-        error_messages={
-            'invalid': 'Enter a valid email address.',
-        }
-    )
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
+    email = forms.EmailField(label="Email")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -111,7 +85,7 @@ class BarberSignUpForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = UserCreationForm.Meta.fields + ('email',)
+        fields = UserCreationForm.Meta.fields + ('email','first_name', 'last_name',)
 
     @transaction.atomic
     def save(self):
--- LineUp/LineUp/settings.py
+++ LineUp/LineUp/settings.py
@@ -86,7 +86,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    ]
+'''
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -99,8 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-]
-
+'''
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
